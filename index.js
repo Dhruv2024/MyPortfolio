4f4725f7dd1f1bffc88f59d8473e91392d5d1c4e
Dhruv <adhruv674@gmail.com>
2024-07-13 21:09:19 +0530

    uploading files

---
+++ index.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Full Stack Developer",
+        "Web Developer",
+        "UI-UX Designer",
+        "Backend Developer",
+        "Coder",
+        "Competitive Programmer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
